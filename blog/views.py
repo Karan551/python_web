22cf4e5deb7bb2781928a651133d5cab2d2cd535
Kartik <iamkaranmishra551@gmail.com>
2023-11-26 09:36:35 +0530

    Adding function in blog views.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("This is a blog Home.")
+
+def blog(request,slug):
+    return HttpResponse(f'This is django blog {slug}')
