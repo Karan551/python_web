6d06c87406996c3ccf610a92bae98bd30ebc6a72
Kartik <iamkaranmishra551@gmail.com>
2023-11-26 23:57:18 +0530

    Render HTML file in our blog app.

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("This is a blog Home.")
+    return render(request,'blog/blogHome.html')
+    # return HttpResponse("This is a blog Home.")
 
 def blog(request,slug):
-    return HttpResponse(f'This is django blog {slug}')
+    return render(request,'blog/blogPost.html')
+    # return HttpResponse(f'This is django blog {slug}')
