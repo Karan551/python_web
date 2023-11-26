b83ab9802b8e2b17d196abad34a0701ba57fe167
Kartik <iamkaranmishra551@gmail.com>
2023-11-26 23:58:56 +0530

    Render HTML file in our home app.

--- home/views.py
+++ home/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("This is a django Home.")
+    return render(request,'home/index.html')
+    # return HttpResponse("This is a django Home.")
 
 def contact(request):
-    return HttpResponse("This is a django contact webpage.")
+    return render(request,'home/contact.html')
+    # return HttpResponse("This is a django contact webpage.")
 
 def about(request):
-    return HttpResponse("This is a django about webpage.")
+    return render(request,'home/about.html')
+    # return HttpResponse("This is a django about webpage.")
