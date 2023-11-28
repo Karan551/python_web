5c2b6e47074d119a33e60bea42ad34b33b4ab15e
Kartik <iamkaranmishra551@gmail.com>
2023-11-28 11:02:20 +0530

    Create contact table.

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Contact(models.Model):
+    serial_number = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=250)
+    user_email = models.CharField(max_length=100)
+    user_phoneNumber = models.CharField(max_length=100)
+    content = models.TextField(max_length=500)
+    timestamp = models.DateTimeField(auto_now_add=True)
