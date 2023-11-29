4d999924601cba2e7c02bce3ca06e0a8714ec81f
Kartik <iamkaranmishra551@gmail.com>
2023-11-29 09:16:09 +0530

    Adding contact details and message framework code.

--- home/views.py
+++ home/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
+from home.models import Contact
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
-    return render(request,'home/index.html')
-    # return HttpResponse("This is a django Home.")
+    return render(request, "home/index.html")
+
 
 def contact(request):
-    return render(request,'home/contact.html')
-    # return HttpResponse("This is a django contact webpage.")
+    if request.method == "POST":
+        name = request.POST["user_name"]
+        email = request.POST["email"]
+        phone = request.POST["contact-number"]
+        content = request.POST["issue"]
+        # If user name is less than two or blank display alert messages
+        if name == " " or len(name) <= 2:
+            messages.error(request, "Please fill form correctly.")
+        else:
+            user_data = Contact(
+                user_name=name,
+                user_email=email,
+                user_phoneNumber=phone,
+                content=content,
+            )
+            user_data.save()
+            messages.success(request, "Your issue has been submitted with us.")
+    return render(request, "home/contact.html")
+
 
 def about(request):
-    return render(request,'home/about.html')
-    # return HttpResponse("This is a django about webpage.")
+    return render(request, "home/about.html")
