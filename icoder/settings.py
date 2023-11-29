82bcf79ab99f2810041d94a40c756a37d314ccc1
Kartik <iamkaranmishra551@gmail.com>
2023-11-29 09:23:10 +0530

    adding overwrite message framework error constant value to danger.

--- icoder/settings.py
+++ icoder/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from django.contrib.messages import constants as messages
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Update the message message framework
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger"
+    
+}
