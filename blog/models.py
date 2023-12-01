0c6dcaa0b722ab5564948bd65e5153b1e4d025ae
Kartik <iamkaranmishra551@gmail.com>
2023-12-01 11:24:24 +0530

    Adding Post table in our database.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    serial_number = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100)
+    content = models.TextField()
+    timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return self.title + " by " + self.author
