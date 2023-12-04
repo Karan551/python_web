8b0f311ccb8edcdce58e9cba28836ab43ab1d388
Kartik <iamkaranmishra551@gmail.com>
2023-12-04 10:45:43 +0530

    change blogHome name to blogHome.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('',views.home,name='blogHome'),
     path('<str:slug>',views.blog,name='blog'),
 ]
